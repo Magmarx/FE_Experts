d24d00c7f4736357032ed0bf707cca91df88e481
Steven <jeremy.monzon@plusti.com>
2019-10-24 15:12:08 -0600

    Última versión

--- Steven_master/index.js
+++ Steven_master/index.js
@@ -1,7 +1,10 @@
+// definición del url donde el json está almacenado
 const url = 'https://next.json-generator.com/api/json/get/4Jfe8YWYP';
 let s = [];
 
+// función que inicia cuando el documento html está listo
 $(document).ready(function ($) {
+    // Definición de propiedades css de mi slider
     var slideCount = $('#slider ul li').length;
     var slideWidth = $('#slider ul li').width();
     var slideHeight = $('#slider ul li').height();
@@ -19,6 +22,7 @@ $(document).ready(function ($) {
 
     $('#slider ul li:last-child').prependTo('#slider ul');
 
+    // Función para mover hacia la izquierda
     function moveLeft() {
         $('#slider ul').animate({
             left: +slideWidth
@@ -28,6 +32,7 @@ $(document).ready(function ($) {
         });
     };
 
+    // Función para mover hacia la derecha
     function moveRight() {
         $('#slider ul').animate({
             left: -slideWidth
@@ -36,7 +41,7 @@ $(document).ready(function ($) {
             $('#slider ul').css('left', '');
         });
     };
-
+    //  Referencia de mis botones en el slider
     $('a.control_prev').click(function () {
         moveLeft();
     });
@@ -52,35 +57,38 @@ function mostrar() {
             return response.json();
         })
         .then(function mostrarjson(mostrar) {
-                let inputName = document.getElementById("nombre"),
-                    inputLast = document.getElementById("apellido"),
-                    inputEdad = document.getElementById("edad"),
-                    inputEmail = document.getElementById("email"),
-                    inputDireccion = document.getElementById("direccion"),
-                    inputActivos = document.getElementById("activos"),
-                    inputFechaRegistro = document.getElementById("fechaR"),
-                    inputOjos = document.getElementById("ojos"),
-                    inputCompania = document.getElementById("compania"),
-                    inputTelefono = document.getElementById("telefono"),
-                    inputFruta = document.getElementById("fruta"),
-                    inputAcerca = document.getElementById("acerca"),
-                    Datos = {
-                        Nombre: inputName.value,
-                        Apellido: inputLast.value,
-                        Edad: inputEdad.value,
-                        Email: inputEmail.value,
-                        Dirección: inputDireccion.value,
-                        Activos: inputActivos.value,
-                        FechaRegistro: inputFechaRegistro.value,
-                        ColorOjos: inputOjos.value,
-                        Compañia: inputCompania.value,
-                        Teléfono: inputTelefono.value,
-                        FrutaFavorita: inputFruta.value,
-                        AcercaDe: inputAcerca.value
-                    };
-                    let datoscon = JSON.stringify(Datos);
-                    let res = datoscon.split(",");
-                    alert(res)
+            // Obtengo los valores del json que me interesa
+            let inputName = document.getElementById("nombre"),
+                inputLast = document.getElementById("apellido"),
+                inputEdad = document.getElementById("edad"),
+                inputEmail = document.getElementById("email"),
+                inputDireccion = document.getElementById("direccion"),
+                inputActivos = document.getElementById("activos"),
+                inputFechaRegistro = document.getElementById("fechaR"),
+                inputOjos = document.getElementById("ojos"),
+                inputCompania = document.getElementById("compania"),
+                inputTelefono = document.getElementById("telefono"),
+                inputFruta = document.getElementById("fruta"),
+                inputAcerca = document.getElementById("acerca"),
+                // Guardo estos valores dentro de otro objeto
+                Datos = {
+                    Nombre: inputName.value,
+                    Apellido: inputLast.value,
+                    Edad: inputEdad.value,
+                    Email: inputEmail.value,
+                    Dirección: inputDireccion.value,
+                    Activos: inputActivos.value,
+                    FechaRegistro: inputFechaRegistro.value,
+                    ColorOjos: inputOjos.value,
+                    Compañia: inputCompania.value,
+                    Teléfono: inputTelefono.value,
+                    FrutaFavorita: inputFruta.value,
+                    AcercaDe: inputAcerca.value
+                };
+            // convierto mi objeto a tipo string y luego lo muestro en pantalla
+            let datoscon = JSON.stringify(Datos);
+            let res = datoscon.split(",");
+            alert(res)
         })
 };
 
@@ -91,10 +99,11 @@ function slider() {
             return response.json();
         })
         .then(mostrar => {
+            // Recorro todos los elementos de mi objeto
             mostrar.forEach(users => {
                 let mainSlider = document.getElementById("mainSlider"),
                     liSlider = document.createElement("LI");
-
+                // Inserto en mi slider los datos que quiero
                 liSlider.innerText = `${users.name.first} ${users.name.last}`
                 liSlider.style = `background-image: url(${users.image}); font-size: 30px;
         color: whitesmoke;`
@@ -105,6 +114,7 @@ function slider() {
 
                     e.preventDefault();
 
+                    // Obtengo los valores del json para luego insertarlos en los inputs
                     let inputName = document.getElementById("nombre"),
                         inputLast = document.getElementById("apellido"),
                         inputEdad = document.getElementById("edad"),
@@ -118,7 +128,7 @@ function slider() {
                         inputFruta = document.getElementById("fruta"),
                         inputAcerca = document.getElementById("acerca"),
                         selectedUser = s.filter(user => (`${user.name.first} ${user.name.last}`) === this.innerText)[0];
-
+                    // El lugar en donde inserto los datos del json
                     inputName.value = selectedUser.name.first;
                     inputLast.value = selectedUser.name.last;
                     inputEdad.value = selectedUser.age;
@@ -137,6 +147,7 @@ function slider() {
         })
 };
 
+// funciones para cambiar de información principal a secundaria y viceversa
 function activar2() {
     let variable = document.getElementById("first"),
         va = document.getElementById("second"),
@@ -163,30 +174,31 @@ function activar() {
     va2.className = 'none';
 }
 
-function borrar(){
+// funcion para borrar datos de los inputs
+function borrar() {
     let inputName = document.getElementById("nombre"),
-                    inputLast = document.getElementById("apellido"),
-                    inputEdad = document.getElementById("edad"),
-                    inputEmail = document.getElementById("email"),
-                    inputDireccion = document.getElementById("direccion"),
-                    inputActivos = document.getElementById("activos"),
-                    inputFechaRegistro = document.getElementById("fechaR"),
-                    inputOjos = document.getElementById("ojos"),
-                    inputCompania = document.getElementById("compania"),
-                    inputTelefono = document.getElementById("telefono"),
-                    inputFruta = document.getElementById("fruta"),
-                    inputAcerca = document.getElementById("acerca");
+        inputLast = document.getElementById("apellido"),
+        inputEdad = document.getElementById("edad"),
+        inputEmail = document.getElementById("email"),
+        inputDireccion = document.getElementById("direccion"),
+        inputActivos = document.getElementById("activos"),
+        inputFechaRegistro = document.getElementById("fechaR"),
+        inputOjos = document.getElementById("ojos"),
+        inputCompania = document.getElementById("compania"),
+        inputTelefono = document.getElementById("telefono"),
+        inputFruta = document.getElementById("fruta"),
+        inputAcerca = document.getElementById("acerca");
 
-                    inputName.value = "";
-                    inputLast.value = "";
-                    inputEdad.value = "";
-                    inputEmail.value = "";
-                    inputDireccion.value = "";
-                    inputActivos.value = "";
-                    inputFechaRegistro.value = "";
-                    inputOjos.value = "";
-                    inputCompania.value = "";
-                    inputTelefono.value = "";
-                    inputFruta.value = "";
-                    inputAcerca.value = "";
+    inputName.value = "";
+    inputLast.value = "";
+    inputEdad.value = "";
+    inputEmail.value = "";
+    inputDireccion.value = "";
+    inputActivos.value = "";
+    inputFechaRegistro.value = "";
+    inputOjos.value = "";
+    inputCompania.value = "";
+    inputTelefono.value = "";
+    inputFruta.value = "";
+    inputAcerca.value = "";
 }
