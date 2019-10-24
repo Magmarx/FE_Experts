cfea72d089b907d02afd80fe8148b3c3d7e63090
selvin <scastro@plusti.com>
2019-10-24 15:09:38 -0600

    cambio 3

--- ejercicio/proyec.js
+++ ejercicio/proyec.js
@@ -1,2 +1,30 @@
-const url = 'https://next.json-generator.com/api/json/get/4Jfe8YWYP'
-console.log('element.json')
+// const url = 'https://next.json-generator.com/api/json/get/4Jfe8YWYP'
+// console.log('element.json')
+let users = []
+
+$(document).ready(function ($) {
+
+    let button = document.getElementById("getStarted");
+
+    $(button).on('click', function (e) {
+      
+      
+        e.preventDefault();
+
+        let fname = document.getElementById("fname"),
+            lname = document.getElementById("lname"),
+            iedad = document.getElementById("iedad"),
+            icorreo = document.getElementById("icorreo"),
+            genero = document.getElementById("genero"),
+            jsonData = {
+                fname: fname.value,
+                lname: lname.value,
+                iedad: iedad.value,
+                icorreo: icorreo.value,
+                genero: genero.value,
+                
+            };
+
+        alert(JSON.stringify(jsonData));
+
+      });
